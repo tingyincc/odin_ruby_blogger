934f95c40c20eb7ee83fe664b58372e5caaa67ff
ccty <tingyin.cc@gmail.com>
2020-07-22 22:00:53 +0800

    form-based workflow

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+class ArticlesController < ApplicationController
+    include ArticlesHelper
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        # @article.title = params[:article][:title]
+        # @article.body = params[:article][:body]
+        @article.save
+        flash.notice = "New Article '#{@article.title}' Saved!"
+        redirect_to article_path(@article)
+        #redirect_to articles_path()
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        flash.notice = "Article '#{@article.title}' Deleted!"
+        @article.destroy
+        redirect_to articles_path
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+        redirect_to article_path(@article)
+    end
+
+end
